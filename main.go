80078d8b60162fb241b100d18d62a19dace2ee4b
Nyame-Wolf <mumenyam@gmail.com>
2023-08-16 21:25:05 +0300

    use an infinite for loop to prompt the user to book for more tickets

--- main.go
+++ main.go
@@ -22,29 +22,31 @@ func main() {
 	// create a welcome message for conference attendees.
 	fmt.Printf("Welcome to the %v. There are a total of  %v tickets and only %v tickets remaining\n", conferenceName, remainingTickets, conferenceTickets)
 
-	// get back user unput
-	fmt.Println("Enter your first name to book:")
-	fmt.Scan(&firstName)
-	fmt.Println("Enter your last name")
-	fmt.Scan(&lastName)
-	fmt.Println("Enter your email address")
-	fmt.Scan(&email)
-	fmt.Println("Enter number of tickets")
-	fmt.Scan(&userTickets)
+	for {
+		// get back user unput
+		fmt.Println("Enter your first name to book:")
+		fmt.Scan(&firstName)
+		fmt.Println("Enter your last name")
+		fmt.Scan(&lastName)
+		fmt.Println("Enter your email address")
+		fmt.Scan(&email)
+		fmt.Println("Enter number of tickets")
+		fmt.Scan(&userTickets)
 
-	// calculate remaining tickets:
-	remainingTickets = remainingTickets - userTickets
+		// calculate remaining tickets:
+		remainingTickets = remainingTickets - userTickets
 
-	//store new users in slice
-	bookings = append(bookings, firstName+" "+lastName)
+		//store new users in slice
+		bookings = append(bookings, firstName+" "+lastName)
 
-	// see test bookings in slice
-	fmt.Printf("Slice value %v\n", bookings)
-	fmt.Printf("The type is %T\n", bookings)
+		// see test bookings in slice
+		fmt.Printf("Slice value %v\n", bookings)
+		fmt.Printf("The type is %T\n", bookings)
 
-	fmt.Printf("Thank you  %v %v for booking %v tickets. You will receive a confirmation at %v\n", firstName, lastName, userTickets, email)
-	fmt.Printf("%v Remaining tickets\n", remainingTickets)
+		fmt.Printf("Thank you  %v %v for booking %v tickets. You will receive a confirmation at %v\n", firstName, lastName, userTickets, email)
+		fmt.Printf("%v Remaining tickets\n", remainingTickets)
 
-	fmt.Printf("This are all of our bookings %v", bookings)
+		fmt.Printf("This are all of our bookings %v\n", bookings)
+	}
 
 }
